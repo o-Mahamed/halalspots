aa6237a2552cec9f4dd01eb457cb0e70c966241d
Liban213 <157420372+Liban213@users.noreply.github.com>
2024-02-18 11:33:03 -0500

    Create Rest.js

---
+++ app/Rest.js
@@ -0,0 +1,25 @@
+var Scarbs_restaurants = {
+    "Burgers / Fries": ["Nadi Halal Kebab House", "B Boyz", "The Bullguer", "Adams Halal burger"],
+    "Pizza / Wings": ["Pizza Land Halal", "Al Madina Halal Pizza & Bakery", "Pizza night", "Scarborough Pizza Kabob"],
+    "Somali Food": [ "Sahan Restaurant", "New Banadir Restaurant"],
+    "Shawarma Spots": ["Sda"],
+    "Desert / Cafe": ["Sunkris Desert Palace", "Dessert corner", "Creme et miel"]
+};
+
+
+var Etob_restaurants = {
+    "Burgers / Fries": ["Scarborough", "B Boyz", "The Bullguer", "Adams Halal burger"],
+    "Pizza / Wings": ["Pizza Land Halal", "Al Madina Halal Pizza & Bakery", "Pizza night", "Scarborough Pizza Kabob"],
+    "Somali Food": [ "Sahan Restaurant", "New Banadir Restaurant"],
+    "Shawarma Spots": ["Sda"],
+    "Desert / Cafe": ["Sunkris Desert Palace", "Dessert corner", "Creme et miel"]
+};
+
+var Dt_restaurants = {
+    "Burgers / Fries": ["Scarborough", "B Boyz", "The Bullguer", "Adams Halal burger"],
+    "Pizza / Wings": ["Pizza Land Halal", "Al Madina Halal Pizza & Bakery", "Pizza night", "Scarborough Pizza Kabob"],
+    "Somali Food": [ "Sahan Restaurant", "New Banadir Restaurant"],
+    "Shawarma Spots": ["Sda"],
+    "Desert / Cafe": ["Sunkris Desert Palace", "Dessert corner", "Creme et miel"]
+};
+
